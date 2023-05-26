7a152a2bb4c4cb58edb4ebdc2de215f6c493cb17
vovkaooo <vovkaooo22@gmail.com>
2023-05-26 22:32:24 +0300

    ссылки

--- go.go
+++ go.go
@@ -1,41 +1,33 @@
 package main
 
-var noLocal int = 3 //не локальная переменная
-func localint() {
-
-	var local int = 2 //локальная переменная
-	println(local)
-
-	var enterf interface{}         // создание переменной типо интерфейс без присвоения
-	var enterf_2 = enterf          // создание переменной без указания типа типа интерфейс с присвоением интерфейса
-	var enterf_3 interface{} = nil // идентичная запись
-	println(enterf_2)
-	println(enterf_3)
-
-	/*nil - нужно указывать всегда для ссылочных типов данных
-	таких как срезы, указатели, отображений, каналов, функций
-	это гарантирует, что переменная имеет значения*/
-
-	var s string
-	println(s) //выведет пустую строку, а не ошибку
-
-	var v, t, e = true, "te", 123
-	println(v, t, e)
-
-	short := 123 //краткое обьявление
-	println(short)
-
-	ter, mo, reg := 1, 2, 3
-	println(ter, mo, reg)
-
-	//f,err:= os.OpenFile("rt.tr",1,4)
-	//println(f,err)
-
-	//f,err:= os.OpenFile("rt.tr",1,4)
-	//println(f,err)
-	// Будет ошибка, так как два раза инициализируем
-}
-
+var global *int // глобальная ссылка на переменную
 func main() {
-	localint()
+
+	i_1 := 1    // инициализиурем переменную типа int
+	p_1 := &i_1 // создаем ссылку на переменную
+
+	println(p_1) // выводим ссылку
+	println(i_1) // выводим значение переменной
+
+	i_2 := 2      //инициализиурем переменную типа int
+	println(&i_2) //выводим ссылку переменной
+	println(i_2)  //выводим саму переменную
+
+	p_2 := new(int) // выделяем память и возвращаем адрес
+	println(p_2)    //выводим ссылку в памяти
+	println(*p_2)   //выводим данные по сылке
+	*p_2 = 3        //присваиваем по ссылке новое значение в памяти
+	println(*p_2)   //выводим новое значение
+
+	type structy struct {
+		str string
+		ss  string
+	}
+
+	var struc_1 structy = structy{
+		str: "ss",
+		ss:  "34",
+	}
+	println(struc_1.ss)
+
 }
